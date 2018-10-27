b7a7c3010e6b63dae29bf80d60809e61ba34257d
Arif Hosan <hosan.arif0@gmail.com>
2018-10-27 18:29:03 +0600

    Basic Django setup

---
+++ django_basic/local_settings.py
@@ -0,0 +1,13 @@
+import os
+from .settings import BASE_DIR
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
